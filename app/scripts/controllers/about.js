d1f9bac653dbc74066b3d3050cdc34b5bbf60f71
uf0 <scnepa222@gmail.com>
2016-11-08 12:19:38 +0100

    bootstrap project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ancoraApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ancoraApp
+ */
+angular.module('ancoraApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ancoraApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ancoraApp
+ */
+angular.module('ancoraApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
