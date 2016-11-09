b57fd9a735d3653e4c0783fdb5cf9ff3f580f2e4
uf0 <scnepa222@gmail.com>
2016-11-09 23:03:23 +0100

    added covers

--- app/scripts/controllers/cover.js
+++ app/scripts/controllers/cover.js
@@ -8,10 +8,18 @@
  * Controller of the ancoraApp
  */
 angular.module('ancoraApp')
-  .controller('CoverCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('CoverCtrl', function ($scope, $routeParams, $sce) {
+    $scope.area = $routeParams.area;
+    $scope.mdUrl = 'texts/' + $routeParams.area + '.md';
+    $scope.onPlayerReady = function(API) {
+      $scope.videoAPI = API;
+      $scope.videoAPI.setVolume(0);
+    };
+
+    $scope.videoconfig = {
+      sources: [
+        {src: $sce.trustAsResourceUrl("videos/"+$scope.area+".mp4"), type: "video/mp4"}
+      ]
+    }
+
   });
