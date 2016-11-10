ef2fc805f4ce02ff70c20bceb07be71ee9ba94ca
uf0 <scnepa222@gmail.com>
2016-11-10 10:30:59 +0100

    epilogo

--- app/scripts/controllers/epilogo.js
+++ app/scripts/controllers/epilogo.js
@@ -8,10 +8,17 @@
  * Controller of the ancoraApp
  */
 angular.module('ancoraApp')
-  .controller('EpilogoCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EpilogoCtrl', function ($scope, $routeParams, $sce) {
+
+    $scope.title = 'Milano scoperta:<br>riaprire i Navigli<br>per una città cartolina'
+    $scope.onPlayerReady = function(API) {
+      $scope.videoAPI = API;
+      $scope.videoAPI.setVolume(0);
+    };
+
+    $scope.videoconfig = {
+      sources: [
+        {src: $sce.trustAsResourceUrl("videos/epilogo.mp4"), type: "video/mp4"}
+      ]
+    }
   });
